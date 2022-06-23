67df48294c87c8a62e32e1404ff409115ebf7f12
Chi Kim <chi@kim.club>
2022-06-23 08:10:59 -0700

    adds markdoc and first page

--- index.js
+++ index.js
@@ -3,11 +3,51 @@ const path = require("path");
 const fs = require("fs");
 const multer = require("multer");
 
+const Markdoc = require("@markdoc/markdoc");
+
 const app = express();
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use("/public", express.static("public"));
+
+app.get("/", (req, res) => {
+  console.log(req.ip);
+  console.log(req.hostname);
+  if (req.hostname.split(".").includes("v3")) {
+    return res.send(`
+    <style>
+    * {
+    text-align:center;
+    }
+    </style>
+    <h1>Please Go To</h1>
+    <code>69.181.248.93:8035</code>
+    <hr>
+    
+    <p>
+    Put the above into the url bar like so:
+    </p>
+    <img src="https://v3.amayz.dev/public/photos/uploads/ip-url.png" alt="">
+    <p>Depending on the operating system and browser you use, your browser may look different from the image above.</p>
+    `);
+  }
+  const doc = `
+# Hello world.
+  > My first Markdoc page
+`;
+
+  const ast = Markdoc.parse(doc);
+
+  const content = Markdoc.transform(ast);
+
+  const html = Markdoc.renderers.html(content);
+  res.send(`
+<link rel="stylesheet" href="https://fonts.xz.style/serve/inter.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css">
+  ${html}
+  `);
+});
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -60,8 +100,41 @@ app.get("/files", (req, res) => {
     });
   });
 });
-app.get("/", (req, res) => {
-  res.send("hello");
+
+app.get("/:article", (req, res) => {
+  const doc = `
+# Hello world.
+  > My first Markdoc page
+`;
+
+  const ast = Markdoc.parse(doc);
+
+  const content = Markdoc.transform(ast);
+
+  const html = Markdoc.renderers.html(content);
+  res.send(`
+<link rel="stylesheet" href="https://fonts.xz.style/serve/inter.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css">
+  ${html}
+  `);
+});
+
+app.get("/:article/edit", (req, res) => {
+  const doc = `
+# Hello world.
+  > My first Markdoc page
+`;
+
+  const ast = Markdoc.parse(doc);
+
+  const content = Markdoc.transform(ast);
+
+  const html = Markdoc.renderers.html(content);
+  res.send(`
+<link rel="stylesheet" href="https://fonts.xz.style/serve/inter.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css">
+  ${html}
+  `);
 });
 
 app.listen(process.env.PORT || 8123);
