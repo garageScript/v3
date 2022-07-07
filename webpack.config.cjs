3dcba6446e9566898b06279e0c1849d817dcdd21
Chi Kim <chi@kim.club>
2022-07-07 03:34:15 -0700

    adds webpack config

---
+++ webpack.config.cjs
@@ -0,0 +1,31 @@
+// Tutorial: https://aglowiditsolutions.com/blog/react-webpack/
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // Outputs all "import css" into one main.css file
+
+module.exports = {
+  entry: "./src/client.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  mode: "development",
+};
