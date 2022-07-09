d0b971bec1cc4b5dc0e7b02c343869859a173b6a
Chi Kim <chi@kim.club>
2022-07-08 22:49:45 -0700

    refactors localStorage

--- src/lib/localStorage.js
+++ src/lib/localStorage.js
@@ -1,42 +1,45 @@
 const STORAGE_KEY = "exercise";
 const exerciseData = JSON.parse(localStorage.getItem(STORAGE_KEY) || "{}");
 
+const getExerciseInfo = (articleName, exerciseId) => {
+  const articleExerciseStat = exerciseData[articleName] || {};
+  return articleExerciseStat[exerciseId] || {};
+};
+
+const setExerciseInfo = (articleName, exerciseId, exerciseInfo) => {
+  const articleExerciseStat = exerciseData[articleName] || {};
+
+  articleExerciseStat[exerciseId] = { ...exerciseInfo };
+  exerciseData[articleName] = articleExerciseStat;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(exerciseData));
+};
+
 export const exerciseStat = {
   getCorrectCount: (articleName, exerciseId) => {
-    const articleExerciseStat = exerciseData[articleName] || {};
-    const exerciseInfo = articleExerciseStat[exerciseId] || {};
+    const exerciseInfo = getExerciseInfo(articleName, exerciseId);
 
     return exerciseInfo.correctCount || 0;
   },
 
   hasNotCompleted: (articleName, exerciseId) => {
-    const articleExerciseStat = exerciseData[articleName] || {};
-    const exerciseInfo = articleExerciseStat[exerciseId] || {};
+    const exerciseInfo = getExerciseInfo(articleName, exerciseId);
 
     return !exerciseInfo.isComplete;
   },
 
   completeExercise: (articleName, exerciseId) => {
-    const articleExerciseStat = exerciseData[articleName] || {};
-    const exerciseInfo = articleExerciseStat[exerciseId] || {};
+    const exerciseInfo = getExerciseInfo(articleName, exerciseId);
 
     exerciseInfo.isComplete = true;
-    articleExerciseStat[exerciseId] = exerciseInfo;
-    exerciseData[articleName] = articleExerciseStat;
-
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(exerciseData));
+    setExerciseInfo(articleName, exerciseId, exerciseInfo);
   },
 
   increaseCorrectCount: (articleName, exerciseId) => {
-    const articleExerciseStat = exerciseData[articleName] || {};
-    const exerciseInfo = articleExerciseStat[exerciseId] || {};
+    const exerciseInfo = getExerciseInfo(articleName, exerciseId);
 
     const correctCount = exerciseInfo.correctCount || 0;
-
     exerciseInfo.correctCount = correctCount + 1;
-    articleExerciseStat[exerciseId] = exerciseInfo;
-    exerciseData[articleName] = articleExerciseStat;
-
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(exerciseData));
+    setExerciseInfo(articleName, exerciseId, exerciseInfo);
   },
 };
